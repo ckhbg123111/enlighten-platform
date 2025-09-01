1b183f32d0410a816104cb1f7c8860b79a7ba599
khchen <3170105539@jue.edu.cn>
2025-09-01 18:10:18 +0800

    初始化用户

--- enlighten-platform-web/src/main/resources/db/migration/V21__create_initial_users.sql
+++ enlighten-platform-web/src/main/resources/db/migration/V21__create_initial_users.sql
@@ -7,52 +7,52 @@ USE `enlighten_platform`;
 -- 妇科用户（2个）
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_G_F_01' AS `username`, MD5('123456') AS `password`, 'admin_g_f_01@gzrmyy.com' AS `email`, '13800000001' AS `phone`, 1 AS `status`, 'USER' AS `role`, '赣州人民医院' AS `hospital`, '妇科' AS `department`
+    SELECT 'admin01' AS `username`, MD5('123456') AS `password`, 'admin_g_f_01@gzrmyy.com' AS `email`, '13800000001' AS `phone`, 1 AS `status`, 'USER' AS `role`, '赣州人民医院' AS `hospital`, '妇科' AS `department`
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_F_01');
 
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_G_F_02', MD5('123456'), 'admin_g_f_02@gzrmyy.com', '13800000002', 1, 'USER', '赣州人民医院', '妇科'
+    SELECT 'admin02', MD5('123456'), 'admin_g_f_02@gzrmyy.com', '13800000002', 1, 'USER', '赣州人民医院', '妇科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_F_02');
 
 -- 宣传科用户（2个）
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_G_X_01', MD5('123456'), 'admin_g_x_01@gzrmyy.com', '13800000003', 1, 'USER', '赣州人民医院', '宣传科'
+    SELECT 'admin03', MD5('123456'), 'admin_g_x_01@gzrmyy.com', '13800000003', 1, 'USER', '赣州人民医院', '宣传科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_X_01');
 
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_G_X_02', MD5('123456'), 'admin_g_x_02@gzrmyy.com', '13800000004', 1, 'USER', '赣州人民医院', '宣传科'
+    SELECT 'admin04', MD5('123456'), 'admin_g_x_02@gzrmyy.com', '13800000004', 1, 'USER', '赣州人民医院', '宣传科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_X_02');
 
 -- 信息科用户（2个）
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_G_I_01', MD5('123456'), 'admin_g_i_01@gzrmyy.com', '13800000005', 1, 'USER', '赣州人民医院', '信息科'
+    SELECT 'admin05', MD5('123456'), 'admin_g_i_01@gzrmyy.com', '13800000005', 1, 'USER', '赣州人民医院', '信息科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_I_01');
 
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_G_I_02', MD5('123456'), 'admin_g_i_02@gzrmyy.com', '13800000006', 1, 'USER', '赣州人民医院', '信息科'
+    SELECT 'admin06', MD5('123456'), 'admin_g_i_02@gzrmyy.com', '13800000006', 1, 'USER', '赣州人民医院', '信息科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_I_02');
 
 -- 肿瘤科用户（2个）
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_G_Z_01', MD5('123456'), 'admin_g_z_01@gzrmyy.com', '13800000007', 1, 'USER', '赣州人民医院', '肿瘤科'
+    SELECT 'admin07', MD5('123456'), 'admin_g_z_01@gzrmyy.com', '13800000007', 1, 'USER', '赣州人民医院', '肿瘤科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_Z_01');
 
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_G_Z_02', MD5('123456'), 'admin_g_z_02@gzrmyy.com', '13800000008', 1, 'USER', '赣州人民医院', '肿瘤科'
+    SELECT 'admin08', MD5('123456'), 'admin_g_z_02@gzrmyy.com', '13800000008', 1, 'USER', '赣州人民医院', '肿瘤科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_Z_02');
 
@@ -60,51 +60,51 @@ WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_G_Z_02');
 -- 妇科用户（2个）
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_O_F_01', MD5('123456'), 'admin_o_f_01@other.com', '13800000009', 1, 'USER', '其他医院', '妇科'
+    SELECT 'admin09', MD5('123456'), 'admin_o_f_01@other.com', '13800000009', 1, 'USER', '其他医院', '妇科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_O_F_01');
 
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_O_F_02', MD5('123456'), 'admin_o_f_02@other.com', '13800000010', 1, 'USER', '其他医院', '妇科'
+    SELECT 'admin10', MD5('123456'), 'admin_o_f_02@other.com', '13800000010', 1, 'USER', '其他医院', '妇科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_O_F_02');
 
 -- 宣传科用户（2个）
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_O_X_01', MD5('123456'), 'admin_o_x_01@other.com', '13800000011', 1, 'USER', '其他医院', '宣传科'
+    SELECT 'admin11', MD5('123456'), 'admin_o_x_01@other.com', '13800000011', 1, 'USER', '其他医院', '宣传科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_O_X_01');
 
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_O_X_02', MD5('123456'), 'admin_o_x_02@other.com', '13800000012', 1, 'USER', '其他医院', '宣传科'
+    SELECT 'admin12', MD5('123456'), 'admin_o_x_02@other.com', '13800000012', 1, 'USER', '其他医院', '宣传科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_O_X_02');
 
 -- 信息科用户（2个）
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_O_I_01', MD5('123456'), 'admin_o_i_01@other.com', '13800000013', 1, 'USER', '其他医院', '信息科'
+    SELECT 'admin13', MD5('123456'), 'admin_o_i_01@other.com', '13800000013', 1, 'USER', '其他医院', '信息科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_O_I_01');
 
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_O_I_02', MD5('123456'), 'admin_o_i_02@other.com', '13800000014', 1, 'USER', '其他医院', '信息科'
+    SELECT 'admin14', MD5('123456'), 'admin_o_i_02@other.com', '13800000014', 1, 'USER', '其他医院', '信息科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_O_I_02');
 
 -- 肿瘤科用户（2个）
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_O_Z_01', MD5('123456'), 'admin_o_z_01@other.com', '13800000015', 1, 'USER', '其他医院', '肿瘤科'
+    SELECT 'admin15', MD5('123456'), 'admin_o_z_01@other.com', '13800000015', 1, 'USER', '其他医院', '肿瘤科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_O_Z_01');
 
 INSERT INTO `user` (`username`, `password`, `email`, `phone`, `status`, `role`, `hospital`, `department`)
 SELECT * FROM (
-    SELECT 'admin_O_Z_02', MD5('123456'), 'admin_o_z_02@other.com', '13800000016', 1, 'USER', '其他医院', '肿瘤科'
+    SELECT 'admin16', MD5('123456'), 'admin_o_z_02@other.com', '13800000016', 1, 'USER', '其他医院', '肿瘤科'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin_O_Z_02');
